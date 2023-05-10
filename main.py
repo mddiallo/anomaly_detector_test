7ae9f8b02c9c0910062f21bd9eafe1e866b061d4
Mouhamadou DIALLO <mouhamadou.diallo@hotmail.com>
2023-05-10 11:33:05 +0000

    add main

---
+++ main.py
@@ -0,0 +1,5 @@
+# import
+
+import os
+from azure.cognitiveservices.anomalydetector import AnomalyDetectorClient
+from azure.cognitiveservices.anomalydetector.models import DetectRequest, TimeSeriesPoint
